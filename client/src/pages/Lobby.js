85d43c09a8af1621b86c2cfb89fa06931a050bef
David Wang <18davidwang@gmail.com>
2023-01-11 15:06:34 -0800

    Merge branch

--- client/src/pages/Lobby.js
+++ client/src/pages/Lobby.js
@@ -1,34 +1,32 @@
 import "./Lobby.css";
-import React, { useEffect } from "react";
-import { useState } from "react";
-import io from "socket.io-client";
+import React from "react";
 import { useNavigate } from "react-router-dom";
-import character_img0 from "../images/sword_icon_img1.png";
-// import character_img1 from "../images/sword_icon_img2.png";
-// import character_img2 from "../images/sword_icon_img3.png";
-// import character_img3 from "../images/sword_icon_img4.png";
-// import character_img4 from "../images/sword_icon_img5.png";
+
 
 export function Lobby(props) {
-  // const navigate = useNavigate();
-  console.log(props);
+  const navigate = useNavigate();
+  console.log("lobby props:", props);
 
-  const { player1, player2, player3, player4 } = props;
+  const { player1, player2, player3, player4, room } = props;
 
   function startGame() {
-    // navigate(`/game/${roomId}`);
+    console.log("start room:", room);
+    navigate(`/game/${room}`);
   }
 
   return (
     <div className="Lobby-container">
       <div className="Lobby-avatar">
         <label>{player1}</label>
+        <br />
         <label>{player2}</label>
+        <br />
         <label>{player3}</label>
+        <br />
         <label>{player4}</label>
       </div>
       <div className="Lobby-button">
-        <button className="Start" onClick={startGame}>
+        <button id="start-game-button" onClick={startGame}>
           Start Game
         </button>
       </div>
--- server/index.js
+++ server/index.js
@@ -19,9 +19,11 @@ const io = new Server(server, {
   },
 });
 
-let rooms = {}; // roomid1 {[names1,name2]}
-let roomId, playerName = "";
+const PLAYER_LIMIT = 4;
+let rooms = {}; // roomid: { players:[name], sockets[id]}
+let roomId, playerName, socketId = "";
 
+<<<<<<< HEAD
 io.on("connection", (socket) => {
   console.log(`User connected: ${socket.id}`);
   // listening to create_room
@@ -47,13 +49,39 @@ io.on("connection", (socket) => {
       console.log("JoinGame",rooms)
       socket.emit("validate", true); // Home.js
       io.to(roomId).emit("JoinPlayers", rooms[roomId]); // Lobby.js
+=======
+io.on("connection", (client) => {
+  console.log(`User connected: ${client.id}`);
+
+  client.on("create_room", handleCreateRoom);
+  client.on("join_room", handleJoinRoom);
+  client.on("disconnect", () => { client.leave(roomId); });
+
+  function handleCreateRoom(data_room_player) {
+    roomId = data_room_player[0];
+    playerName = data_room_player[1];
+    socketId = client.id;
+    rooms[roomId]={players:[], sockets:[]};
+    rooms[roomId]["players"].push(playerName);
+    rooms[roomId]["sockets"].push(socketId);
+    console.log("Createroom data", rooms);
+    client.join(roomId);
+    io.to(roomId).emit("InitPlayerRoom", [playerName, roomId]);
+  }
+  function handleJoinRoom(data_room_player) {
+    roomId = data_room_player[0];
+    playerName = data_room_player[1];
+    socketId = client.id;
+    console.log("playerlength", rooms[roomId]["players"].length);
+    if (rooms[roomId] && rooms[roomId]["players"].length < PLAYER_LIMIT) {
+      rooms[roomId]["players"].push(playerName);
+      rooms[roomId]["sockets"].push(socketId);
+      console.log("Joinroom data",rooms);
+      client.join(roomId);
+      client.emit("validate_room", true); // Validate room is full / existed
+      io.to(roomId).emit("JoinedPlayers", [rooms[roomId], Object.keys(rooms)[0]]);
+>>>>>>> 7a1e047 (Adding different players on map)
     } else {
-      socket.emit("validate", false);
-    }
-    // listening to disconnect
-    socket.on("disconnect", () => {
-      console.log(`Client ${socket.id} disconnected`);
-      socket.leave(roomId);
-    })
-  }); 
+      client.emit("validate_room", false);
+    }}
 });
