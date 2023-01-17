14b0d913b48b3d31ce2ea33fb47f95c4ec572360
Andy Farquharson <andyfarquharson@gmail.com>
2023-01-16 18:42:55 -0800

    Add css to lobby and display roomID

--- client/src/App.js
+++ client/src/App.js
@@ -7,55 +7,53 @@ import Game from "./pages/Game";
 import io from "socket.io-client";
 import background from "./sounds/backgroundMusic.mp3";
 
-
 const socket = io.connect("http://localhost:3001");
 
 function App(props) {
   const [user, setUser] = useState("");
   const [players, setPlayers] = useState({});
+  const [room, setRoom] = useState("");
   let song = new Audio(background);
 
   useEffect(() => {
-    socket.on("room_joined",(playerList, room) => {
+    socket.on("room_joined", (playerList, room) => {
       console.log("room_joined line 27:", playerList);
+      setRoom(room);
       updatePlayerList(playerList, room);
       socket.emit("joined_lobby", room);
     });
-  })
+  });
   function updatePlayerList(playerList, room) {
     const newPlayers = {};
     playerList.forEach((player) => {
-      return newPlayers[player.socket] = {
+      return (newPlayers[player.socket] = {
         playerId: player.socket,
         playerName: player.playerName,
-        roomId: room
-      }
-    })
-    setPlayers((prev) => ({  
+        roomId: room,
+      });
+    });
+    setPlayers((prev) => ({
       ...prev,
       ...newPlayers,
-    }))
+    }));
   }
 
-
-  
   return (
     <main className="main">
-
-    <Routes>
-      <Route path="/" element={<Home 
-        socket={socket}
-        user={user}
-        setUser={setUser} 
-        song={song}
-        />}/>
-      <Route path="/lobby/:id" element={<Lobby
-      socket={socket}
-      players={players} />}/>
-      <Route path="/game/:id" element={<Game socket={socket} />} />
-    </Routes>
-  </main>
-    
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Home socket={socket} user={user} setUser={setUser} song={song} />
+          }
+        />
+        <Route
+          path="/lobby/:id"
+          element={<Lobby room={room} socket={socket} players={players} />}
+        />
+        <Route path="/game/:id" element={<Game socket={socket} />} />
+      </Routes>
+    </main>
   );
 }
 
